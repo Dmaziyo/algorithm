2e1f26190b9ba19e39561d3f672e89945d35a693
D城兔Maziyo <247948681@qq.com>
2023-04-01 15:45:05 +0800

    Updated the algorithm by including a comment for the unique bst.ts file

--- algorithm/binary tree/Unique Binary Search Trees.ts
+++ algorithm/binary tree/Unique Binary Search Trees.ts
@@ -3,10 +3,12 @@
  * TODO review
  */
 function numTrees(n: number): number {
-  if (n <= 1) return n
-  let dp: number[] = [1, 1]
-  for (let i = 2; i <= n; i++) {
+  let dp = [1, 1, 2]
+  if (n <= 2) return dp[n]
+
+  for (let i = 3; i <= n; i++) {
     dp[i] = 0
+    // j = 1 means let the minimum number be the root initially
     for (let j = 1; j <= i; j++) {
       dp[i] += dp[j - 1] * dp[i - j]
     }
